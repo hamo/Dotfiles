416fa6e101b069209bf052fdcb1c2d8b14860bda
Yang Bai <hamo.by@gmail.com>
2014-10-07 17:42:46 +0800

    Fix company-go and enable go-eldoc

--- .emacs.d/init.d/programming/go.el
+++ .emacs.d/init.d/programming/go.el
@@ -1,4 +1,11 @@
 (local/package-install 'go-mode)
 (local/package-install 'company-go)
+(local/package-install 'go-eldoc)
 
-(require 'company-go)
+(use-package go-mode
+  :init
+  (progn
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (add-hook 'go-mode-hook 'go-eldoc-setup)
+    (add-hook 'go-mode-hook (lambda ()
+      (set (make-local-variable 'company-backends) '(company-go))))))
--- .emacs.d/init.d/programming/scala.el
+++ .emacs.d/init.d/programming/scala.el
@@ -1,3 +1,2 @@
 (local/package-install 'scala-mode2)
 (local/package-install 'sbt-mode)
-(local/package-install 'ensime)
