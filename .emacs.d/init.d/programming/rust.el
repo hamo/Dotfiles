fa55a6f4e0026ce08dad418bef328c94061f713d
Yang Bai <hamo.by@gmail.com>
2016-07-12 18:12:27 +0800

    add rust config

--- .emacs.d/init.d/programming/rust.el
+++ .emacs.d/init.d/programming/rust.el
@@ -1,1 +1,19 @@
 (local/package-install 'rust-mode)
+(local/package-install 'cargo)
+(local/package-install 'racer)
+
+(use-package rust-mode
+  :init
+  (progn
+    (add-hook 'rust-mode-hook 'cargo-minor-mode)))
+
+(use-package racer
+  :init
+  (progn
+    (add-hook 'rust-mode-hook 'racer-mode)
+    (add-hook 'racer-mode-hook 'eldoc-mode))
+  :config
+  (progn
+    ;; (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+    (setq racer-rust-src-path "~/workspace/rust/rust-lang/src") ;; Rust source code PATH
+    ))
