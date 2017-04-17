c1525236d9d157893ef0da11c7134241a23c4283
Yang Bai <hamo.by@gmail.com>
2017-04-17 13:42:43 +0800

    emacs: add go-guru

--- .emacs.d/init.d/programming/go.el
+++ .emacs.d/init.d/programming/go.el
@@ -1,11 +1,13 @@
 (local/package-install 'go-mode)
 (local/package-install 'go-eldoc)
 (local/package-install 'company-go)
+(local/package-install 'go-guru)
 
 (use-package go-mode
   :init
   (progn
     (add-hook 'go-mode-hook (lambda ()
 			      (go-eldoc-setup)
-			      (set (make-local-variable 'company-backends) '(company-go company-yasnippet))))
+			      (set (make-local-variable 'company-backends) '(company-go company-yasnippet))
+			      (go-guru-hl-identifier-mode)))
     (add-hook 'before-save-hook 'gofmt-before-save)))
