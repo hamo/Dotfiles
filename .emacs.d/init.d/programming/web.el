fe9a220e87abeae137457c30ad25f1807ec75557
Yang Bai <hamo.by@gmail.com>
2017-09-29 11:28:37 +0800

    emacs: support php

--- .emacs.d/init.d/programming/web.el
+++ .emacs.d/init.d/programming/web.el
@@ -1,6 +1,27 @@
 (local/package-install 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(use-package web-mode
+  :config
+  (progn
+    (setq indent-tabs-mode nil)
+    (setq web-mode-markup-indent-offset 2
+	  web-mode-css-indent-offset 2
+	  web-mode-code-indent-offset 2)))
 
 (local/package-install 'coffee-mode)
 
 (local/package-install 'slim-mode)
+
+(local/package-install 'php-mode)
+(local/package-install 'company-php)
+(use-package php-mode
+  :config
+  (progn
+    (setq indent-tabs-mode nil)
+    (setq c-basic-offset 2)
+    (setq php-template-compatibility nil))
+  :init
+  (progn
+    (subword-mode 1)
+    (add-to-list 'company-backends 'company-ac-php-backend)
+    ))
