1cdc1588246d154d3ebeb6655a4c52b475007f54
Yang Bai <yang.bai@grabtaxi.com>
2020-02-29 22:48:12 +0800

    emacs.d: add exec-path-from-shell for mac os

--- .emacs.d/init.d/basic.el
+++ .emacs.d/init.d/basic.el
@@ -14,6 +14,15 @@
 ;;; replace highlighted text with what I type rather than just inserting at point
 (delete-selection-mode t)
 
+;;; for Mac OS, read and set env from shell
+(use-package exec-path-from-shell
+  :if (memq window-system '(ns mac))
+  :ensure t
+  :config
+  (setq exec-path-from-shell-arguments '("-l"))
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "GOROOT" "GOPATH"))
+  (exec-path-from-shell-initialize))
+
 ;;; start emacs as a server
 (require 'server)
 (when (and (>= emacs-major-version 23)
