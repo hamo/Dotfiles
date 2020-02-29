c7158cc1b99b21595f34e235ce9b179337fa698a
Yang Bai <yang.bai@gmail.com>
2020-02-29 18:56:49 +0800

    emacs.d: update rust

--- .emacs.d/init.d/programming/rust.el
+++ .emacs.d/init.d/programming/rust.el
@@ -1,19 +1,19 @@
 (local/package-install 'rust-mode)
 (local/package-install 'cargo)
 (local/package-install 'racer)
+(local/package-install 'flycheck-rust)
 
 (use-package rust-mode
   :init
   (progn
-    (add-hook 'rust-mode-hook 'cargo-minor-mode)))
+    (add-hook 'rust-mode-hook 'cargo-minor-mode))
+  :config
+  (setq
+   rust-format-on-save t
+   ))
 
 (use-package racer
   :init
   (progn
     (add-hook 'rust-mode-hook 'racer-mode)
-    (add-hook 'racer-mode-hook 'eldoc-mode))
-  :config
-  (progn
-    ;; (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
-    (setq racer-rust-src-path "~/workspace/rust/rust-lang/src") ;; Rust source code PATH
-    ))
+    (add-hook 'racer-mode-hook 'eldoc-mode)))
