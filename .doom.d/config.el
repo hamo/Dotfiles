45adac661f32fa5871dc219a7da27ea3b64b4cab
Yang Bai <baiyang.001@bytedance.com>
2022-08-18 11:44:13 +0800

    doom-emacs: enable global word wrap mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -23,7 +23,7 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-(setq doom-font (font-spec :family "Source Code Pro" :size 16))
+(setq doom-font (font-spec :family "Source Code Pro" :size 18))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -77,3 +77,5 @@
 ;; they are implemented.
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(+global-word-wrap-mode +1)
